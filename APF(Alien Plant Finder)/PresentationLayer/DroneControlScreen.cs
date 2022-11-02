6bd23c10a76ac7bedc401ab02ed0f2a419dd96a6
Ruhan Potgieter <ruhan@sietechnn.co.za>
2022-11-02 19:14:43 +0200

    Add project files.

---
+++ APF(Alien Plant Finder)/PresentationLayer/DroneControlScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APF_Alien_Plant_Finder_.PresentationLayer
+{
+    public partial class DroneControlScreen : Form
+    {
+        public DroneControlScreen()
+        {
+            Thread t = new Thread(new ThreadStart(StartForrm));
+            t.Start();
+            Thread.Sleep(5000);
+            InitializeComponent();
+            t.Abort();
+        }
+
+        public void StartForrm()
+        {
+            Application.Run(new SplashScreen());
+        }
+    }
+}
