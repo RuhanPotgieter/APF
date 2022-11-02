08e11735cea8702228d35daa14d82ea93e73c224
Ruhan Potgieter <ruhan@sietechnn.co.za>
2022-11-03 00:25:10 +0200

    edited the two models

--- APF(Alien Plant Finder)/BusinessLogicLayer/Models/BluetoothConnectivity.cs
+++ APF(Alien Plant Finder)/BusinessLogicLayer/Models/BluetoothConnectivity.cs
@@ -8,34 +8,7 @@ namespace APF_Alien_Plant_Finder_.BusinessLogicLayer
 {
     internal class BluetoothConnectivity
     {
-        private string _bluetoothName;
-        private string _bluetoothAddress;
-        private string _bluetoothPin;
-
-        public string BluetoothName
-        {
-            get { return _bluetoothName; }
-            set { _bluetoothName = value; }
-        }
-
-        public string BluetoothAddress
-        {
-            get { return _bluetoothAddress; }
-            set { _bluetoothAddress = value; }
-        }
-
-        public string BluetoothPin
-        {
-            get { return _bluetoothPin; }
-            set { _bluetoothPin = value; }
-        }
-
-        public BluetoothConnectivity(string bluetoothName, string bluetoothAddress, string bluetoothPin)
-        {
-            _bluetoothName = bluetoothName;
-            _bluetoothAddress = bluetoothAddress;
-            _bluetoothPin = bluetoothPin;
-        }
+        
 
 
 
