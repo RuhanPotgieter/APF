fbdeb7b43d10c1d9a7649d56ce3a37c6fe5c6bef
Kotodama <muhammadabam@gmail.com>
2022-11-08 16:00:27 +0200

    ML model class
    created class and fixed DAL

---
+++ APF(Alien Plant Finder)/DataAccessLayer/MLConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APF_Alien_Plant_Finder_
+{
+    internal class MLConnection
+    {
+
+    }
+}
